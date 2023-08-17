296c49ef7b677dd5d0ddc2f5b69d562ea107bd51
Karjalaz <kella1997@mail.ru>
2023-08-18 02:54:46 +0300

    added deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/chat-bot/',
+  base: '/chat-bot/',
   plugins: [vue()],
   resolve: {
     alias: {
