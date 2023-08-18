798018011ea15d2844de2f2cb755beb42864d009
Karjalaz <kella1997@mail.ru>
2023-08-18 03:10:01 +0300

    Revert "added deployment workflow"
    This reverts commit 296c49ef7b677dd5d0ddc2f5b69d562ea107bd51.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chat-bot/',
+  publicPath: '/chat-bot/',
   plugins: [vue()],
   resolve: {
     alias: {
